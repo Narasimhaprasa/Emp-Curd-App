41ca7c4dfd5eef6631443a268e6b33ba1e69dca5
Lakshmi Narasimhaprasad <92583589+Narasimhaprasa@users.noreply.github.com>
2023-08-20 11:27:39 +0530

    Update BootMvcProj07MiniprojectCurDopertionsApplication.java

--- src/main/java/com/nt/BootMvcProj07MiniprojectCurDopertionsApplication.java
+++ src/main/java/com/nt/BootMvcProj07MiniprojectCurDopertionsApplication.java
@@ -3,6 +3,7 @@ package com.nt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class BootMvcProj07MiniprojectCurDopertionsApplication {
